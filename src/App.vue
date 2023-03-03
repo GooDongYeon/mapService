8e4390d555f9c404596d443e3c5961fcfbe217e6
GooDongYeon <121007224+GooDongYeon@users.noreply.github.com>
2023-03-03 17:19:53 +0900

    [진행중] KakaoMap api 연동 및 구현 vue2.js로 진행 vue2 -> vue3 문법으로 변경 예정

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h3>kakao map demo(center, level)</h3>
+    <div class="controll">
+      <button @click="zoom(1)">
+        <i class="fa-solid fa-plus"></i>
+      </button>
+      <button @click="zoom(-1)">
+        <i class="fa-solid fa-minus"></i>
+      </button>
+    </div>
+    <KakaoMap
+      class="kmap"
+      :options="mapOption" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import KakaoMap from './components/map/KakaoMap.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    KakaoMap,
+  },
+  data() {
+    return {
+      mapOption: {
+        center: {
+          lat: 35.1795543,
+          lng: 129.0756416,
+        },
+        level: 8,
+      },
+    }
+  },
+  mounted() {
+    // let kakao = window.kakao
+    // console.log(this.$refs.map) // should be not null
+
+    // var container = this.$refs.map //지도를 담을 영역의 DOM 레퍼런스
+    // var options = { //지도를 생성할 때 필요한 기본 옵션
+    //   center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    //   level: 6 //지도의 레벨(확대, 축소 정도)
+    // }
+
+    // var mapInstance = new kakao.maps.Map(container, options) //지도 생성 및 객체 리턴
+    // console.log(mapInstance)
+  },
+  methods: {
+    zoom(delta) { //delta is 1 or -1
+      // console.log('[delta]', delta)
+      const level = Math.max(3, this.mapOption.level + delta) // min level 3
+      this.mapOption.level = level
+      console.log(this.mapOption.level)
+    }
   }
 }
 </script>
-
+  
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  border: 1px solid transparent;
+  padding: 6px;
+  background-color: #efefefdd;
+  border-radius: 6px  ;
+}
+button:hover {
+  background-color: #ddd;
+  border-color: #ddd;
+  cursor: pointer;
+}
+button:active {
+  background-color: #aaa;
+  border-color: #aaa;
 }
 </style>
+
---
+++ src/components/map/KakaoMap.vue
@@ -0,0 +1,41 @@
+<template>
+  <div ref="map">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['options'],
+  data() {
+    return {
+      mapInstance: null
+    }
+  },
+  watch: {
+    'options.level'(cur, prev) {
+      console.log(`[LEVEL CHANGD] ${prev} => ${cur}`)
+      this.mapInstance.setLevel(cur)
+    },
+  },
+  mounted() {
+    let kakao = window.kakao
+    console.log(this.$refs.map)
+
+    var container = this.$refs.map
+
+    const { center, level } = this.options
+    this.mapInstance = new kakao.maps.Map(container, {
+      center: new kakao.maps.LatLng(center.lat, center.lng),
+      level,  
+    })
+    // console.log(mapInstance)
+  },
+}
+</script>
+
+<style>
+.kmap {
+  width: 100%;
+  height: 600px;
+}
+</style>
